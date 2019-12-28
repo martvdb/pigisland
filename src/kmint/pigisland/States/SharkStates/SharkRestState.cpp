9e4b3ad9b510eee48569dd69e24ab6d7da392f5b
Mart van den Berg <mberg15@avans.nl>
2019-12-28 01:46:28 +0100

    FSM toegevoegd aan doc en shark rest transitie aangepast

--- src/kmint/pigisland/States/SharkStates/SharkRestState.cpp
+++ src/kmint/pigisland/States/SharkStates/SharkRestState.cpp
@@ -11,29 +11,11 @@ SharkRestState::SharkRestState(kmint::pigisland::shark* shark)
 
 void SharkRestState::Execute(pigisland::shark* shark)
 {
-	if (shark->node().node_id() == pigisland::find_shark_resting_place(shark->graph()).node_id()) {
-		shark->amountOfSteps_ = 0;
-		for (auto i = shark->begin_perceived(); i != shark->end_perceived(); ++i) {
-			if (i->type() == "boat")
-			{
-				if (distance(i->location(), shark->location()) <= 50)
-				{
-					shark->setState(new SharkFleeState(shark));
-					return;
-				}
-
-			}
-			if (i->type() == "pig")
-			{
-				shark->setState(new SharkChaseState(shark));
-				return;
-			}
-		}
-		shark->setState(new SharkWanderState(shark));
-	}
-	else {
 		int next_index = calculateRoute(shark->node().node_id(), pigisland::find_shark_resting_place(shark->graph()).node_id(), shark->graph());
 		shark->node(shark->node()[next_index].to());
-	}
+
+		if (shark->node().node_id() == pigisland::find_shark_resting_place(shark->graph()).node_id()) {
+			shark->setState(new SharkWanderState(shark));
+		}
 }
 
