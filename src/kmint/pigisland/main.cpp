ff480290a0f858f1dfedb00007c23db751804de7
Mart van den Berg <mberg15@avans.nl>
2019-12-29 14:20:19 +0100

    fix main

--- src/kmint/pigisland/main.cpp
+++ src/kmint/pigisland/main.cpp
@@ -33,7 +33,7 @@ int main() {
                                   graphics::image{map.background_image()});
   s.build_actor<play::map_actor>(math::vector2d{0.f, 0.f}, map.graph());
   s.build_actor<pigisland::boat>(graph, pigisland::find_node_of_kind(graph, '1'));
-  s.build_actor<pigisland::shark>(graph, pigisland::find_node_of_kind(graph, 'K'));
+  s.build_actor<pigisland::shark>(graph, s, pigisland::find_node_of_kind(graph, 'K'));
 
   auto locs = pigisland::random_pig_locations(100);
   for(auto loc : locs) {
